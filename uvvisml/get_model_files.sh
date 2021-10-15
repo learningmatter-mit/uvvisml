1072d2576cf565306b12e7a2ebeacd8e18ee07f0
Kevin Greenman <kpg@mit.edu>
2021-10-15 15:28:32 -0400

    get latest version of model files from Zenodo

--- uvvisml/get_model_files.sh
+++ uvvisml/get_model_files.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-wget https://zenodo.org/record/5498399/files/models.tar.gz # Download model file archive from Zenodo
+wget https://zenodo.org/record/5573027/files/models.tar.gz # Download model file archive from Zenodo
 tar -xzf models.tar.gz
 rm models.tar.gz
