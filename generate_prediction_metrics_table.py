15793d22e6172332f85c55f2eb86f505ab2fdc3e
Elenna Kim <kimele2003@gmail.com>
2022-05-03 13:53:42 -0400

    generating table with prediction metrics

--- generate_prediction_metrics_table.py
+++ generate_prediction_metrics_table.py
@@ -1,10 +1,10 @@
-from os import listdir
-from os.path import isfile, join
+import pandas as pd
 
-mypath = "./prediction_plots_images"
-onlyfiles = sorted([f for f in listdir(mypath) if isfile(join(mypath, f))])
+df = pd.read_csv('prediction_metrics.csv').round({'rmse': 3, 'mae': 3, 'r2': 3})
 
-fd = open('prediction_plots_table.md', 'w')
+fd = open('prediction_metrics_table.md', 'w')
+
+fd.write('<font size="1">legend: $RMSE/MAE/R^2$</font>\n')
 
 col_names = ['abs peakwavs max', 'emi peakwavs max', 'abs bandwidth', 'abs molar ext coeff', 'emi bandwidth', 'quantum yield', 'log lifetime']
 col_names_full = ['maximum absorption wavelength', 'maximum emission wavelength', 'absorption bandwidth', 'absorption molar extinction coefficient', 'emission bandwidth', 'quantum yield', 'log lifetime']
@@ -20,19 +20,19 @@ fd.write(col_names_str)
 
 col_header = ''
 for i in range(len(col_names) + 1):
-    col_header += '| --- '
+    col_header += '| :---: '
 col_header += '|\n'
 fd.write(col_header)
 
 for s in split_types:
     row = '| <font size="1">**' + s.replace('_', ' ') + '**</font> '
     for o in opt_props:
-        cell = ''
-        for f in onlyfiles:
-            if s in f and o in f:
-                # cell += ' ' + '![](./prediction_plots_images/' + f + ')'
-                cell += '<img src="./prediction_plots_images/' + f + '" width="50">'
-        row += '|' + cell + ' '
+        cell = '<font size="1">'
+        for index, col in df.iterrows():
+            if col['property'] == o and col['split'] == s:
+                combo_metrics = '**' + str(col['combo']) + ':** ' + str(col['rmse']) + '/' + str(col['mae']) + '/' + str(col['r2']) + ' '
+                cell += combo_metrics
+        row += '|' + cell + '</font> '
     row += '|\n'
     fd.write(row)
 
